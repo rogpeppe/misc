40d4cea400a0bd44edc22b8dc7c83689ba4caeef
Roger Peppe <rogpeppe@gmail.com>
2015-11-24 19:45:44 +0000

    test
    Change-Id: I58601be9ecef74f5c62ac4fab179b9cffb0c8ddc

---
+++ mobile/basic/main.go
@@ -0,0 +1,223 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build darwin linux
+
+// An app that draws a green triangle on a red background.
+//
+// Note: This demo is an early preview of Go 1.5. In order to build this
+// program as an Android APK using the gomobile tool.
+//
+// See http://godoc.org/golang.org/x/mobile/cmd/gomobile to install gomobile.
+//
+// Get the basic example and use gomobile to build or install it on your device.
+//
+//   $ go get -d golang.org/x/mobile/example/basic
+//   $ gomobile build golang.org/x/mobile/example/basic # will build an APK
+//
+//   # plug your Android device to your computer or start an Android emulator.
+//   # if you have adb installed on your machine, use gomobile install to
+//   # build and deploy the APK to an Android target.
+//   $ gomobile install golang.org/x/mobile/example/basic
+//
+// Switch to your device or emulator to start the Basic application from
+// the launcher.
+// You can also run the application on your desktop by running the command
+// below. (Note: It currently doesn't work on Windows.)
+//   $ go install golang.org/x/mobile/example/basic && basic
+package main
+
+import (
+	"encoding/binary"
+	"log"
+	"math"
+	"sync"
+	"time"
+
+	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/event/lifecycle"
+	"golang.org/x/mobile/event/paint"
+	"golang.org/x/mobile/event/size"
+	"golang.org/x/mobile/exp/app/debug"
+	"golang.org/x/mobile/exp/f32"
+	"golang.org/x/mobile/exp/gl/glutil"
+	"golang.org/x/mobile/gl"
+)
+
+var (
+	images   *glutil.Images
+	fps      *debug.FPS
+	program  gl.Program
+	position gl.Attrib
+	offset   gl.Uniform
+	color    gl.Uniform
+	buf      gl.Buffer
+
+	green float32
+	state data
+)
+
+func main() {
+	app.Main(func(a app.App) {
+		var glctx gl.Context
+		var sz size.Event
+		for e := range a.Events() {
+			switch e := a.Filter(e).(type) {
+			case lifecycle.Event:
+				switch e.Crosses(lifecycle.StageVisible) {
+				case lifecycle.CrossOn:
+					glctx, _ = e.DrawContext.(gl.Context)
+					onStart(glctx)
+					a.Send(paint.Event{})
+				case lifecycle.CrossOff:
+					onStop(glctx)
+					glctx = nil
+				}
+			case size.Event:
+				sz = e
+				sizePt := point{float32(sz.WidthPx), float32(sz.HeightPx)}
+				initialPt := point{sizePt.x / 2, sizePt.y / 2}
+				state.setTouch(initialPt)
+				go touchSetter(initialPt, sizePt)
+			case paint.Event:
+				if glctx == nil || e.External {
+					// As we are actively painting as fast as
+					// we can (usually 60 FPS), skip any paint
+					// events sent by the system.
+					continue
+				}
+
+				onPaint(glctx, sz)
+				a.Publish()
+				// Drive the animation by preparing to paint the next frame
+				// after this one is shown.
+				a.Send(paint.Event{})
+				//			case touch.Event:
+				//				state.setTouch(point{e.X, e.Y})
+			default:
+				log.Printf("unrecognised event %#v", e)
+			}
+		}
+	})
+}
+
+func touchSetter(initial, size point) {
+	t0 := time.Now()
+	for {
+		totalTime := time.Since(t0)
+		m := float64(totalTime) / float64(time.Second)
+		p := point{initial.x + float32(math.Cos(float64(m)))*(size.x/2), initial.y + float32(math.Sin(float64(m)))*(size.y/2)}
+		state.setTouch(p)
+		time.Sleep(20 * time.Millisecond)
+	}
+}
+
+func onStart(glctx gl.Context) {
+	var err error
+	program, err = glutil.CreateProgram(glctx, vertexShader, fragmentShader)
+	if err != nil {
+		log.Printf("error creating GL program: %v", err)
+		return
+	}
+
+	buf = glctx.CreateBuffer()
+	glctx.BindBuffer(gl.ARRAY_BUFFER, buf)
+	glctx.BufferData(gl.ARRAY_BUFFER, triangleData, gl.STATIC_DRAW)
+
+	position = glctx.GetAttribLocation(program, "position")
+	color = glctx.GetUniformLocation(program, "color")
+	offset = glctx.GetUniformLocation(program, "offset")
+
+	images = glutil.NewImages(glctx)
+	fps = debug.NewFPS(images)
+}
+
+func onStop(glctx gl.Context) {
+	glctx.DeleteProgram(program)
+	glctx.DeleteBuffer(buf)
+	fps.Release()
+	images.Release()
+}
+
+func onPaint(glctx gl.Context, sz size.Event) {
+	glctx.ClearColor(1, 0, 0, 1)
+	glctx.Clear(gl.COLOR_BUFFER_BIT)
+
+	glctx.UseProgram(program)
+
+	green += 0.01
+	if green > 1 {
+		green = 0
+	}
+	glctx.Uniform4f(color, 0, green, 0, 1)
+
+	touchPt := state.currentTouch()
+	glctx.Uniform2f(offset, touchPt.x/float32(sz.WidthPx), touchPt.y/float32(sz.HeightPx))
+
+	glctx.BindBuffer(gl.ARRAY_BUFFER, buf)
+	glctx.EnableVertexAttribArray(position)
+	glctx.VertexAttribPointer(position, coordsPerVertex, gl.FLOAT, false, 0, 0)
+	glctx.DrawArrays(gl.TRIANGLES, 0, vertexCount)
+	glctx.DisableVertexAttribArray(position)
+
+	fps.Draw(sz)
+}
+
+var triangleData = f32.Bytes(binary.LittleEndian,
+	0.0, 0.4, 0.0, // top left
+	0.0, 0.0, 0.0, // bottom left
+	0.4, 0.0, 0.0, // bottom right
+)
+
+const (
+	coordsPerVertex = 3
+	vertexCount     = 3
+)
+
+const vertexShader = `#version 100
+uniform vec2 offset;
+
+attribute vec4 position;
+void main() {
+	// offset comes in with x/y values between 0 and 1.
+	// position bounds are -1 to 1.
+	vec4 offset4 = vec4(2.0*offset.x-1.0, 1.0-2.0*offset.y, 0, 0);
+	gl_Position = position + offset4;
+}`
+
+const fragmentShader = `#version 100
+precision mediump float;
+uniform vec4 color;
+void main() {
+	gl_FragColor = color;
+}`
+
+type point struct {
+	x, y float32
+}
+
+func (p point) mul(m float32) point {
+	return point{p.x * m, p.y * m}
+}
+
+func (p point) add(p1 point) point {
+	return point{p.x + p1.x, p.y + p1.y}
+}
+
+type data struct {
+	mu      sync.Mutex
+	touchPt point
+}
+
+func (d *data) currentTouch() point {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return d.touchPt
+}
+
+func (d *data) setTouch(p point) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.touchPt = p
+}
